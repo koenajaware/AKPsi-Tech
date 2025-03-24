7297008978326c6108da6950b15842be37f5c8ef
noraaren <aaren3017@gmail.com>
2025-03-23 23:15:26 -0400

    finsih sponsor bar

--- akpsi-tech-site/src/components/Home/Sponsors.jsx
+++ akpsi-tech-site/src/components/Home/Sponsors.jsx
@@ -1,10 +1,25 @@
-import React from 'react';
-import '../../styles/Home/Sponsors.css';
+import React from "react";
+import "../../styles/Home/Sponsors.css";
 
 const Sponsors = () => {
   return (
     <div className="blue-bar">
-      <h2 className="blue-bar-text">Sponsors</h2>
+      <h2>Sponsors</h2>
+      <div className="sponsor-slider">
+        <div className="sponsor-track">
+          <img src="/sponsors/sponsor1.png" alt="Sponsor 1" />
+          <img src="/sponsors/sponsor2.png" alt="Sponsor 2" />
+          <img src="/sponsors/sponsor3.png" alt="Sponsor 3" />
+          <img src="/sponsors/sponsor4.png" alt="Sponsor 4" />
+          <img src="/sponsors/sponsor5.png" alt="Sponsor 5" />
+          {/* Duplicate logos for continuous scrolling */}
+          <img src="/sponsors/sponsor1.png" alt="Sponsor 1" />
+          <img src="/sponsors/sponsor2.png" alt="Sponsor 2" />
+          <img src="/sponsors/sponsor3.png" alt="Sponsor 3" />
+          <img src="/sponsors/sponsor4.png" alt="Sponsor 4" />
+          <img src="/sponsors/sponsor5.png" alt="Sponsor 5" />
+        </div>
+      </div>
     </div>
   );
 };
