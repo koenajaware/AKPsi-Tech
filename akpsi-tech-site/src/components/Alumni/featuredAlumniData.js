a13f02ae2b2f909a2102c4b7b88d217aae63aafd
mwalker1234 <mwalker3155@gmail.com>
2025-04-28 18:07:12 -0400

    alumni page

--- akpsi-tech-site/src/components/Alumni/featuredAlumniData.js
+++ akpsi-tech-site/src/components/Alumni/featuredAlumniData.js
@@ -1,19 +1,24 @@
+import RahulVinod from "../../assets/alumni/RahulVinod.jpg";
+import RasaLogo from "../../assets/alumni/RasaLogo.jpg";
+import DavidMolot from "../../assets/alumni/DavidMolot.jpg";
+import HotglueLogo from "../../assets/alumni/HotglueLogo.jpg";
+
 export const featured = [
     {
       name: "Rahul Vinod",
-      photo: 'https://via.placeholder.com/150?text=Featured+Alumni+Photo+1',
+      photo: RahulVinod,
       role: "Co‑Founder of Rasa",
-      classYear: "Alpha '' Class",
-      logo: 'https://via.placeholder.com/150?text=Logo+1',
-      companyDescription: "Rasa is an open source conversational AI framework…",
+      classYear: "Delta Class",
+      logo: RasaLogo,
+      companyDescription: "Rasa is a fast-casual Indian Restaurant offering vibrant, healthy wraps and bowls",
       quote: "AKPsi taught me how to build real products with real teams."
     },
     {
       name: "David Molot",
-      photo: 'https://via.placeholder.com/150?text=Featured+Alumni+Photo+2',
+      photo: DavidMolot,
       role: "Co‑Founder of Hotglue (YC 21)",
       classYear: "Alpha '' Class",
-      logo: 'https://via.placeholder.com/150?text=Logo+2',
+      logo: HotglueLogo,
       companyDescription: "Hotglue is an embedded integration stack for high‑performing B2B teams.",
       quote: "Joining AKPsi gave me my first taste of real‑world teamwork and product iteration."
     },
