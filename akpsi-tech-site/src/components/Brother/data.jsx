b5838559c832a104ac60bea50d270a1ef43ea52b
Tej <t11l2mtl@gmail.com>
2025-05-01 17:56:17 -0400

    added brothers

---
+++ akpsi-tech-site/src/components/Brother/data.jsx
@@ -0,0 +1,332 @@
+export const chapters = [
+  {
+    "name": "Alpha Kappa",
+    "members": [
+      {
+        "id": 1,
+        "name": "Rohan Gudwani",
+        "imageUrl": "https://drive.google.com/open?id=1Ze-3N8iwgXikPt6rLUe5QD_Cq8WgG9dS"
+      },
+      {
+        "id": 2,
+        "name": "Eileen Chen",
+        "imageUrl": "https://drive.google.com/open?id=1xeqFkSMG6YA9_LcBybd2WVoRn4WhuDrI"
+      },
+      {
+        "id": 3,
+        "name": "Meilin Yuan",
+        "imageUrl": "https://drive.google.com/open?id=1gcCqQFU-BI3whROjxATNU1WWlQpWXEEZ"
+      },
+      {
+        "id": 4,
+        "name": "Jimmy Herman",
+        "imageUrl": "https://drive.google.com/open?id=1c1ZGvotf3v45frsEQrcc1Wpmpm4rETTV"
+      },
+      {
+        "id": 5,
+        "name": "Chris moon",
+        "imageUrl": "https://drive.google.com/open?id=1wHppiyMAwiSWIKPCoGigosPuDlMv39xB"
+      },
+      {
+        "id": 6,
+        "name": "Jiayi Wang",
+        "imageUrl": "https://drive.google.com/open?id=1zuWeo921Yw7tvDRugdMuNUKXI11bhY_X"
+      },
+      {
+        "id": 7,
+        "name": "Abhinav Mukherjee",
+        "imageUrl": "https://drive.google.com/open?id=1ey6Sy8QW4ditFreG3Rdw7Kcdd_OBVPm5"
+      },
+      {
+        "id": 8,
+        "name": "SoEun Kim",
+        "imageUrl": "https://drive.google.com/open?id=18dJqlBtX75wf2WSl2AiwHWzY0Hmte_Tc"
+      },
+      {
+        "id": 9,
+        "name": "Alex Dong",
+        "imageUrl": "https://drive.google.com/open?id=1QYbTdroelpKSPwZxVQ28zM7MMwcPf4O_"
+      },
+      {
+        "id": 10,
+        "name": "Marvi Shroff",
+        "imageUrl": "https://drive.google.com/open?id=1kJK3zge_EqsLSKxVul_AVxSgFlfx1Fj7"
+      }
+    ]
+  },
+  {
+    "name": "Alpha Lambda",
+    "members": [
+      {
+        "id": 1,
+        "name": "Alma Hagstrom",
+        "imageUrl": "https://drive.google.com/open?id=1-8_jnJESwUJP6bJ8-o-V0kofH39EvaM2"
+      },
+      {
+        "id": 2,
+        "name": "Jason Jia",
+        "imageUrl": "https://drive.google.com/open?id=1dGxp8RnnyyJ5PkkYJ-Kg8tmGpeUPbgxe"
+      },
+      {
+        "id": 3,
+        "name": "Juhi Parikh",
+        "imageUrl": "https://drive.google.com/open?id=1TZkP6YuyGEb6rFp5KMunrJ6_8_CTHtD3"
+      },
+      {
+        "id": 4,
+        "name": "Angela Luo",
+        "imageUrl": "https://drive.google.com/open?id=1FQwrG9DBBzbq2GXRyZwl3hXsajj4QyUu"
+      },
+      {
+        "id": 5,
+        "name": "Arun Crispino",
+        "imageUrl": "https://drive.google.com/open?id=1c3npOrkFR6aO0AFVBZRdzl5imsW4rH4H"
+      },
+      {
+        "id": 6,
+        "name": "Nikhail Mann",
+        "imageUrl": "https://drive.google.com/open?id=1xIsoxs5lxfIE1y-m3ZrZ5PhF9567eZkf"
+      },
+      {
+        "id": 7,
+        "name": "Benjamin Weatherhead",
+        "imageUrl": "https://drive.google.com/open?id=1NzAh2zS1RaBetBbKrg6uFD1Bhv1OTmrF"
+      },
+      {
+        "id": 8,
+        "name": "Rahul Harish",
+        "imageUrl": "https://drive.google.com/open?id=1JmNc6K2VeMJGCs9nYRVzDa46fFqC_guI"
+      }
+    ]
+  },
+  {
+    "name": "Alpha Mu",
+    "members": [
+      {
+        "id": 1,
+        "name": "Uzochi Njiaju",
+        "imageUrl": "https://drive.google.com/open?id=19zayvasF8u5bFQQflOUqlUIm6CfRDjFv"
+      },
+      {
+        "id": 2,
+        "name": "Sydney Richman",
+        "imageUrl": "https://drive.google.com/open?id=1kSo8K78gUGzoPQHzHP13PR3gm0dmtH6_"
+      },
+      {
+        "id": 3,
+        "name": "Walid Khokhar",
+        "imageUrl": "https://drive.google.com/open?id=1GXBIocQ5cRidxlW1Kdu2Euwch5LbG8C-"
+      },
+      {
+        "id": 4,
+        "name": "Jake Hofmann",
+        "imageUrl": "https://drive.google.com/open?id=1ejgbSDDRGBvuJO7AoTnBvYhY6YX2CCvo"
+      },
+      {
+        "id": 5,
+        "name": "Fynn Anderson",
+        "imageUrl": "https://drive.google.com/open?id=1UO0ZWBoUFPv_38fyJO7urPVA24p6kLer"
+      },
+      {
+        "id": 6,
+        "name": "Arsha Garg",
+        "imageUrl": "https://drive.google.com/open?id=1fK_V0m30AGIW7nQzsVaFQjfgi6OT74YK"
+      },
+      {
+        "id": 7,
+        "name": "Ryne Acheson",
+        "imageUrl": "https://drive.google.com/open?id=1rzuhNQEVeFt_3EGp-vhN_L8RqoV_ARB1"
+      },
+      {
+        "id": 8,
+        "name": "Madelyn Morales",
+        "imageUrl": "https://drive.google.com/open?id=1o3kK_t4Ei8_duqpRlgZhhoAIAgwlx4DQ"
+      },
+      {
+        "id": 9,
+        "name": "Olevia Tinpaw",
+        "imageUrl": "https://drive.google.com/open?id=13hFap5OoFE8mhGeo2D4HsbQNEB4DdP7D"
+      },
+      {
+        "id": 10,
+        "name": "Eileen Zhang",
+        "imageUrl": "https://drive.google.com/open?id=1K-ELzRQvGBDXGZlmTmY4uym9eIl1KsG_"
+      }
+    ]
+  },
+  {
+    "name": "Alpha Nu",
+    "members": [
+      {
+        "id": 1,
+        "name": "Jeffery Wei",
+        "imageUrl": "https://drive.google.com/open?id=1ZIoG2M0nzMQ7C7-oU63EZ0WwujA-JtNm"
+      },
+      {
+        "id": 2,
+        "name": "Nihar Xavier",
+        "imageUrl": "https://drive.google.com/open?id=18W6L2foI9CFMfy1Y-Y_qActT7waHYWp6"
+      },
+      {
+        "id": 3,
+        "name": "Hasan Ege",
+        "imageUrl": "https://drive.google.com/open?id=1egRdhYOQiPE7_IktZw6YyPqsXE-uo0Ce"
+      },
+      {
+        "id": 4,
+        "name": "Aria Zutshi",
+        "imageUrl": "https://drive.google.com/open?id=1urqpT09xi58OE95KYTT5EJP00Ncf7yb1"
+      },
+      {
+        "id": 5,
+        "name": "Alexis Iwuchukwu",
+        "imageUrl": "https://drive.google.com/open?id=1hOcOM4T0znlz2NqGvBsbRz8m3nv7Vb24"
+      },
+      {
+        "id": 6,
+        "name": "Nancy",
+        "imageUrl": "https://drive.google.com/open?id=1nchx9dzm8J-snH3WzzGXLvlPKVihnr5o"
+      },
+      {
+        "id": 7,
+        "name": "Ayush Gawane",
+        "imageUrl": "https://drive.google.com/open?id=1qpeEdAoJ07YPQv46rlfzuhLjUxCZvVtO"
+      },
+      {
+        "id": 8,
+        "name": "Hamid Shabbir",
+        "imageUrl": "https://drive.google.com/open?id=112KHjdhU56_8a2YXMC1_OxlivRz-seya"
+      },
+      {
+        "id": 9,
+        "name": "Jo\u00e3o Eduardo Camargo de Miranda",
+        "imageUrl": "https://drive.google.com/open?id=1gPylTyTUXeSbPMt91gpkqiCTq4lcHcau"
+      },
+      {
+        "id": 10,
+        "name": "Yash Gulati",
+        "imageUrl": "https://drive.google.com/open?id=14TAP8Qr7zjyQTQwQYZJFffV2PuV3Yavg"
+      },
+      {
+        "id": 11,
+        "name": "Ritika Pokharel",
+        "imageUrl": "https://drive.google.com/open?id=1vOIlR0B6oVV6ijw6jAxt-1l7oxqPLoR1"
+      },
+      {
+        "id": 12,
+        "name": "Megan Walker",
+        "imageUrl": "https://drive.google.com/open?id=1Eo-X_3sxeiM02mg91ggxRTLdyco_PfnL"
+      },
+      {
+        "id": 13,
+        "name": "Bryan Haddad",
+        "imageUrl": "https://drive.google.com/open?id=1MOffcRAsx0wAka15LEvZ9ErWWN9SdL0G"
+      },
+      {
+        "id": 14,
+        "name": "Aditya Waikar",
+        "imageUrl": "https://drive.google.com/open?id=1Wu34-dSIzC4d8Xm48KQXM1Wue3reFuNk"
+      },
+      {
+        "id": 15,
+        "name": "Sreenidhi Yaratha",
+        "imageUrl": "https://drive.google.com/open?id=1w_SL4XfIxB7cQWKYXFEVNs2AF8yPk5lf"
+      }
+    ]
+  },
+  {
+    "name": "Alpha Xi",
+    "members": [
+      {
+        "id": 1,
+        "name": "Shengyu Tan",
+        "imageUrl": "https://drive.google.com/open?id=1T3YDTD9lQryjDO5ZAQXXOFMjGahdhD8J"
+      },
+      {
+        "id": 2,
+        "name": "Agnik Banerjee",
+        "imageUrl": "https://drive.google.com/open?id=1E0jQOn4cNeRPxnXvrnAX-Nfx7kZkqrrJ"
+      },
+      {
+        "id": 3,
+        "name": "Anna Lim",
+        "imageUrl": "https://drive.google.com/open?id=1Vfc-_UIpnzJlXmXNdGfz-qso_4T56568"
+      },
+      {
+        "id": 4,
+        "name": "Hashem Alomar",
+        "imageUrl": "https://drive.google.com/open?id=1sQQ1t4IJFgoYDLLswI8Y9jW9mRCnUVbS"
+      },
+      {
+        "id": 5,
+        "name": "Amir Edris",
+        "imageUrl": "https://drive.google.com/open?id=1GhJA6GoWTGVNsoMxliW025b1UhC4o_KV"
+      },
+      {
+        "id": 6,
+        "name": "Vaishnavi Banda",
+        "imageUrl": "https://drive.google.com/open?id=1e2TQkWfGVoEhFZ4moQyY30rquPritxYi"
+      },
+      {
+        "id": 7,
+        "name": "Ben Henigan",
+        "imageUrl": "https://drive.google.com/open?id=17sNZ5VuSMitPYyH1CqIh5MdwMZZ0_HfN"
+      },
+      {
+        "id": 8,
+        "name": "Riyana Patel",
+        "imageUrl": "https://drive.google.com/open?id=1FhaFBHkOJusnpkczhQBogGXcADFh-ukm"
+      },
+      {
+        "id": 9,
+        "name": "Anya Zhang",
+        "imageUrl": "https://drive.google.com/open?id=1W193PZ-XF4T6OOHGA7jRHkkoOINac-6K"
+      },
+      {
+        "id": 10,
+        "name": "Edwin Cao",
+        "imageUrl": "https://drive.google.com/open?id=1lcJbuHYNyMwQIGPPJ550QzFkMAOEzvgd"
+      },
+      {
+        "id": 11,
+        "name": "Rishi Chudasama",
+        "imageUrl": "https://drive.google.com/open?id=1PEyXPeRq6OunabJzCpnRJOm_TPQ8-_VG"
+      },
+      {
+        "id": 12,
+        "name": "Noah Combatir",
+        "imageUrl": "https://drive.google.com/open?id=10jbCTPF8AL1tFHNlu7lQYgc5GBEnXroq"
+      },
+      {
+        "id": 13,
+        "name": "Bryce Powell",
+        "imageUrl": "https://drive.google.com/open?id=1mHciU-SK8Jq5cWhdt8r8MTzBit97KwQt"
+      },
+      {
+        "id": 14,
+        "name": "Jack Mason",
+        "imageUrl": "https://drive.google.com/open?id=1L4xLqT3jo-ohfMxdCWdaVVBwqzvSUlhV"
+      },
+      {
+        "id": 15,
+        "name": "Alan Deng",
+        "imageUrl": "https://drive.google.com/open?id=1nb9olJg3CnU6DsTllLIEXrqwgLG69TFY"
+      },
+      {
+        "id": 16,
+        "name": "Cayetana (Cate) Areas Barcenas",
+        "imageUrl": "https://drive.google.com/open?id=1j-BlFt_HnEv7rKwMSKYGEfhDj4ll3Etv"
+      }
+    ]
+  },
+  {
+    "name": "Alpha xi",
+    "members": [
+      {
+        "id": 1,
+        "name": "Ryan Krucenski",
+        "imageUrl": "https://drive.google.com/open?id=1Kho22JUc-g9ZdddDHnGubvEkmfSMBY3Q"
+      }
+    ]
+  }
+];
