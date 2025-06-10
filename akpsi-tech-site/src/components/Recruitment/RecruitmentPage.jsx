ed739ecfc490db987ca140a30d7fc231820a79af
adityaswaikar <asw6688@gmail.com>
2025-06-09 22:29:29 -0400

    all added to page and working

--- akpsi-tech-site/src/components/Recruitment/RecruitmentPage.jsx
+++ akpsi-tech-site/src/components/Recruitment/RecruitmentPage.jsx
@@ -2,16 +2,19 @@ import React from 'react';
 import '../../styles/Recruitment/Recruitment.css'; // import your CSS
 import RecruitmentBanner from './RecruitmentBanner';
 import RecruitmentLetter from './RecruitmentLetter';
+import RecruitmentSignup from './RecruitmentSignup';
+import RecruitmentEvents from './RecruitmentEvents';
 
 const RecruitmentPage = () => {
   return (
     <div className="brother-page">
       <header>
         <RecruitmentBanner />
-        <RecruitmentEvents />
       </header>
       <main>
         <RecruitmentLetter />
+        <RecruitmentSignup />
+        <RecruitmentEvents />
       </main>
     </div>
   );
