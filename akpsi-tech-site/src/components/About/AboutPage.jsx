da07490e4d7a5e87eebc916b2c59fa857ae14ec8
RishiAc <rishiac@outlook.com>
2025-06-16 09:58:31 -0400

    added history page

--- akpsi-tech-site/src/components/About/AboutPage.jsx
+++ akpsi-tech-site/src/components/About/AboutPage.jsx
@@ -3,13 +3,14 @@ import Journey from "./Journey";
 import MsgPresident from "./MsgPresident";
 import AboutBanner from "./AboutBanner";
 import SponsorGallery from "./SponsorGallery/SponsorGallery";
-
+import AboutHistory from "./AboutHistory";
 const AboutPage = () => {
     return (
       <div className="about-page">
         <header></header>
         <AboutBanner></AboutBanner>
         <MsgPresident></MsgPresident>
+        <AboutHistory></AboutHistory>
         <Journey></Journey>
         <SponsorGallery></SponsorGallery>
       </div>
