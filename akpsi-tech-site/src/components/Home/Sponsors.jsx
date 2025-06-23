235c02d95efcd1902976129f7183fe6b5bc96009
Patrick Zhang <patrick8751@gmail.com>
2025-06-23 18:42:13 -0400

    home page updates
    added sponsor images
    updated footer links
    updated our story, pillars, awards to match figma design

--- akpsi-tech-site/src/components/Home/Sponsors.jsx
+++ akpsi-tech-site/src/components/Home/Sponsors.jsx
@@ -4,20 +4,20 @@ import "../../styles/Home/Sponsors.css";
 const Sponsors = () => {
   return (
     <div className="blue-bar">
-      <h2>Sponsors</h2>
+      <h2 className="sponsor-header">Sponsors</h2>
       <div className="sponsor-slider">
         <div className="sponsor-track">
-          <img src="/sponsors/sponsor1.png" alt="Sponsor 1" />
-          <img src="/sponsors/sponsor2.png" alt="Sponsor 2" />
-          <img src="/sponsors/sponsor3.png" alt="Sponsor 3" />
-          <img src="/sponsors/sponsor4.png" alt="Sponsor 4" />
-          <img src="/sponsors/sponsor5.png" alt="Sponsor 5" />
+          <img src="/assets/sponsors/CapitalOne.png" alt="Capital One" />
+          <img src="/assets/sponsors/EY.png"  alt="EY" />
+          <img src="/assets/sponsors/Crowe.png" alt="Crowe" />
+          <img src="/assets/sponsors/KPMG.png" alt="KPMG" className="kpmg"/>
+          {/* <img src="/sponsors/sponsor5.png" alt="Sponsor 5" /> */}
           {/* Duplicate logos for continuous scrolling */}
-          <img src="/sponsors/sponsor1.png" alt="Sponsor 1" />
-          <img src="/sponsors/sponsor2.png" alt="Sponsor 2" />
-          <img src="/sponsors/sponsor3.png" alt="Sponsor 3" />
-          <img src="/sponsors/sponsor4.png" alt="Sponsor 4" />
-          <img src="/sponsors/sponsor5.png" alt="Sponsor 5" />
+          <img src="/assets/sponsors/CapitalOne.png" alt="Capital One" />
+          <img src="/assets/sponsors/EY.png" alt="EY" />
+          <img src="/assets/sponsors/Crowe.png" alt="Crowe" />
+          <img src="/assets/sponsors/KPMG.png" alt="KPMG" className="kpmg"/>
+          {/* <img src="/sponsors/sponsor5.png" alt="Sponsor 5" /> */}
         </div>
       </div>
     </div>
