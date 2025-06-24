ca2647a4b06680598a5aaf0c817f8ec1053bfbdb
mwalker1234 <mwalker3155@gmail.com>
2025-06-23 20:21:28 -0400

    sponsor gallery

--- akpsi-tech-site/src/components/About/SponsorGallery/Sponsors.jsx
+++ akpsi-tech-site/src/components/About/SponsorGallery/Sponsors.jsx
@@ -1,23 +1,22 @@
 import React from "react";
 import "../../../styles/About/SponsorGallery/Sponsors.css";
+import solidGray from '../../../assets/sponsors/solid_gray.png';
+import capOne from '../../../assets/sponsors/capital-one-logo.png'
+
 
 const Sponsors = () => {
   return (
-    <div className="blue-bar">
-      <div className="sponsor-slider">
-        <div className="sponsor-track">
-          <img src="/sponsors/sponsor1.png" alt="Sponsor 1" />
-          <img src="/sponsors/sponsor2.png" alt="Sponsor 2" />
-          <img src="/sponsors/sponsor3.png" alt="Sponsor 3" />
-          <img src="/sponsors/sponsor4.png" alt="Sponsor 4" />
-          <img src="/sponsors/sponsor5.png" alt="Sponsor 5" />
-          {/* Duplicate logos for continuous scrolling */}
-          <img src="/sponsors/sponsor1.png" alt="Sponsor 1" />
-          <img src="/sponsors/sponsor2.png" alt="Sponsor 2" />
-          <img src="/sponsors/sponsor3.png" alt="Sponsor 3" />
-          <img src="/sponsors/sponsor4.png" alt="Sponsor 4" />
-          <img src="/sponsors/sponsor5.png" alt="Sponsor 5" />
-        </div>
+    <div className="sponsor-slider">
+      <div className="sponsor-track">
+        <img src={capOne} />
+        <img src={solidGray} />
+        <img src={solidGray} alt="Sponsor 3" />
+        <img src={solidGray} alt="Sponsor 4" />
+        {/* Duplicate logos for continuous scrolling */}
+        <img src={capOne} alt="Sponsor 1" />
+        <img src={solidGray} alt="Sponsor 2" />
+        <img src={solidGray} alt="Sponsor 3" />
+        <img src={solidGray} alt="Sponsor 4" />
       </div>
     </div>
   );
