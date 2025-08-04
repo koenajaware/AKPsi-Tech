3bb135c13c2c17026f5f1d213289de9f9f2fa54d
Patrick Zhang <patrick8751@gmail.com>
2025-08-03 20:48:50 -0400

    Contact Page update

--- akpsi-tech-site/src/components/Contact/contact.jsx
+++ akpsi-tech-site/src/components/Contact/contact.jsx
@@ -1,7 +1,9 @@
 import React, { useEffect, useState } from 'react';
 import { motion as Motion, useScroll, useTransform } from 'framer-motion';
 import '../../styles/Contact/contact.css';
-import ContactImage from '../../assets/ContactUsCover.png'; 
+import ContactImage from '../../assets/ContactUsCover.png';
+import { submit } from './action';
+
 
 const ContactPage = () => {
   return (
@@ -22,13 +24,17 @@ const ContactPage = () => {
                 </a>
               </div>
               <div className="contact-person">
-                <h2>Abhiram Sriramaneni</h2>
+                <h2>Abhiram Sreeramaneni</h2>
                 <p>Vice President of Membership</p>
                 <a href="mailto:akpsiotrush@gmail.com">akpsiotrush@gmail.com</a>
               </div>
+              <div className="contact-person">
+                <h2>Omega Theta Chapter</h2>
+                <a href="mailto:akpsiotrush@gmail.com">akpsiomegatheta@gmail.com</a>
+              </div>
             </div>
             <div className="contact-form-container">
-              <form action="#" method="POST"> {/* NOTE: I need the form handling logic to go here (like the URL) */}
+              <form action={submit} method="POST"> {/* NOTE: I need the form handling logic to go here (like the URL) */}
                 <input type="text" name="fullName" placeholder="Full Name" required />
                 <input type="email" name="email" placeholder="Email Address" required />
                 <input type="text" name="subject" placeholder="Subject Line" required />
